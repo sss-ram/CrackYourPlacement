27cbdd96273176466fdd18023a361015cc7391da
sss-ram <135205831+sss-ram@users.noreply.github.com>
2024-07-29 23:58:11 +0530

    Add files via upload

---
+++ Day1/CoinChange.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        int dp[amount+1];
+        for(int i=0; i<=amount; i++)
+            dp[i] = INT_MAX-1;
+        dp[0] = 0;
+
+        for(int i=1; i<=amount; i++){
+            for(int c: coins){
+                if(i-c>=0)
+                    dp[i] = min(dp[i], dp[i-c]+1);
+            }
+        }
+        return dp[amount]==INT_MAX-1? -1:dp[amount];
+    }
+};
