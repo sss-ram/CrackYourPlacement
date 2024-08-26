b0d7ebca9951f5f8f589a13c85136652c4fa533f
sss-ram <135205831+sss-ram@users.noreply.github.com>
2024-08-26 23:53:10 +0530

    Create searchInRotatedArray.java

---
+++ Day29/searchInRotatedArray.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int n = nums.length;
+        int l = 0, r = n - 1;
+
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+            if (nums[m] == target)
+                return m;
+            if (nums[l] <= nums[m]) {
+                if (nums[l] <= target && target < nums[m]) {
+                    r = m - 1;
+                } else {
+                    l = m + 1;
+                }
+            } 
+            else {
+                if (nums[m] < target && target <= nums[r]) {
+                    l = m + 1;
+                } else {
+                    r = m - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
